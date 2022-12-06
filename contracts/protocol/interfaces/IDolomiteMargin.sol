d6a0634eb1e9f8057c40a9b20782b7711f7c1200
Corey <corey@dolomite.io>
2022-12-06 00:54:51 -0500

    updated IDolomiteMargin to contain ownable functions

--- contracts/protocol/interfaces/IDolomiteMargin.sol
+++ contracts/protocol/interfaces/IDolomiteMargin.sol
@@ -675,4 +675,31 @@ interface IDolomiteMargin {
         bool special
     )
     external;
+
+    // ============ Owner Functions ============
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() external view returns (address);
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() external view returns (bool);
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() external;
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) external;
 }
